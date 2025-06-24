c677a3aa2e520aa6cc13a4ba1ce49101fefe4b39
Tatyana <filippovat42@gmail.com>
2025-06-24 17:08:13 +0300

    привязала к api/ получение данных

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import TodoInput from "./components/todoInput/TodoInput.tsx";
 import css from "./App.module.css";
 import TodoInputCard from "./components/todoCard/TodoInputCard.tsx";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export interface TodoItem {
   id: number;
@@ -14,6 +15,25 @@ export interface TodoItem {
 
 function App() {
   const [todoList, setTodoList] = useState<TodoItem[]>([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:3000/tasks`);
+        console.log(response.data);
+        const items = response.data.map((item: any) => {
+          return { id: item.id, name: item.title, subList: [] };
+        });
+        console.log(items);
+        setTodoList(items);
+      } catch {
+        // setTodoList();
+      } finally {
+        // setTodoList();
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className={css.wrapper}>
       <h1 className={css.title}>To-do list</h1>
